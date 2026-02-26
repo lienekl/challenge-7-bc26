8ee0dfc0d8e42ad1072b9ac3ed5009f1feafd374
lienekl <lienekl@yahoo.com>
2026-02-26 18:21:39 +0000

    working on  code

---
+++ public/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const noteList = document.getElementById("note-list");
+  const inputForm = document.getElementById("input-form");
+  const input = document.getElementById("input");
+
+});
